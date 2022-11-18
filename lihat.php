c5b703da9d54efbb710de26ed0c34cf828354b49
wellaamandaa <103342778+wellaamandaa@users.noreply.github.com>
2022-11-19 00:35:07 +0700

    Add files via upload

---
+++ lihat.php
@@ -0,0 +1,29 @@
+<table border="1">
+    <tr>
+        <th> No. </th>
+        <th> NIM </th>
+        <th> Nama </th>
+        <th> Program Studi </th>
+    </tr>
+    <?php
+    include "database.php";
+
+    $prodi = $_POST['request'];
+
+    $query = "SELECT * FROM mhs WHERE prodi = '$prodi'";
+
+    $result = mysqli_query($connection, $query);
+    $i = 0;
+        while ($data = mysqli_fetch_array($result)) :
+            $i++;
+    ?>
+    <tr>
+        <td> <?= $i ?> </td>
+        <td> <?= $data["nim"] ?> </td>
+        <td> <?= $data["nama"] ?> </td>
+        <td> <?= $data["prodi"] ?> </td>
+    </tr>
+<?php
+endwhile
+?>
+</table>
